9436cc1dc2406352d9518510845d13042ef6e0b5
Lukas Johannes Möller <74617774+JohannesLks@users.noreply.github.com>
2025-03-09 20:28:32 +0100

    bug

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
       <header class="bg-gray-900 sticky top-0 z-50">
         <div class="container mx-auto flex justify-between items-center py-4">
           <div class="flex items-center">
-            <nuxt-link to="/" @click="handleLogoClick"><img src="@/public/red.svg" alt="Pontis IT-Consulting Logo" class="h-14 w-auto mr-4"></nuxt-link>
+            <nuxt-link to="/"><img src="@/public/red.svg" alt="Pontis IT-Consulting Logo" class="h-14 w-auto mr-4"></nuxt-link>
           </div>
           <div class="flex md:hidden z-50">
             <button id="hamburger" class="text-white focus:outline-none" @click="toggleMenu" v-show="!isMenuVisible">
@@ -74,7 +74,7 @@
         <slot />
       </div>
       <footer class="py-10 bg-gray-100 dark:bg-gray-900 text-black dark:text-white">
-        <nuxt-link to="/" @click="handleLogoClick">
+        <nuxt-link to="/" @click.native="scrollToTop">
           <img v-if="!isDarkMode" src="@/public/red.svg" alt="Logo" class="h-14 w-auto mx-auto mb-5">
           <img v-else src="@/public/red.svg" alt="Logo-White" class="h-14 w-auto mx-auto mb-5">
         </nuxt-link>
@@ -112,12 +112,8 @@ export default {
     }
   },
   methods: {
-    handleLogoClick(event) {
-      // Wenn wir bereits auf der Startseite sind, scrollen wir nach oben
-      if (this.$route.path === '/') {
-        event.preventDefault();
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-      }
+    scrollToTop() {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
     },
     toggleMenu() {
       this.isMenuVisible = !this.isMenuVisible;
