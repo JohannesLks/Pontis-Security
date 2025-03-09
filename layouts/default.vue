fb12fa4cc1ade00b5c5a47b1e97fd0c171e52b38
Lukas Johannes Möller <74617774+JohannesLks@users.noreply.github.com>
2025-03-09 18:00:45 +0100

    start adjustmenst logo

--- layouts/default.vue
+++ layouts/default.vue
@@ -4,7 +4,7 @@
       <header class="bg-gray-900 sticky top-0 z-50">
         <div class="container mx-auto flex justify-between items-center py-4">
           <div class="flex items-center">
-            <nuxt-link to="/"><img src="@/public/white.svg" alt="Pontis IT-Consulting Logo" class="h-14 w-auto mr-4"></nuxt-link>
+            <nuxt-link to="/"><img src="@/public/red.svg" alt="Pontis IT-Consulting Logo" class="h-14 w-auto mr-4"></nuxt-link>
           </div>
           <div class="flex md:hidden z-50">
             <button id="hamburger" class="text-white focus:outline-none" @click="toggleMenu" v-show="!isMenuVisible">
@@ -71,8 +71,8 @@
       </div>
       <footer class="py-10 bg-gray-100 dark:bg-gray-900 text-black dark:text-white">
         <nuxt-link to="/" @click.native="scrollToTop">
-          <img v-if="!isDarkMode" src="@/public/black.svg" alt="Logo" class="h-14 w-auto mx-auto mb-5">
-          <img v-else src="@/public/white.svg" alt="Logo-White" class="h-14 w-auto mx-auto mb-5">
+          <img v-if="!isDarkMode" src="@/public/red.svg" alt="Logo" class="h-14 w-auto mx-auto mb-5">
+          <img v-else src="@/public/red.svg" alt="Logo-White" class="h-14 w-auto mx-auto mb-5">
         </nuxt-link>
         <span class="block text-center text-gray-600 dark:text-gray-400 font-semibold">© 2024 Pontis IT-Consulting. Alle Rechte vorbehalten.</span>
         <ul class="flex justify-center mt-10 space-x-8">
